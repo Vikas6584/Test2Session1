c384103ea3d0a8d9feca26d6b1b3b8167595667f
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-02-20 05:15:40 +0000

    completed p1 original and the debug log of p1 as well :))

--- p1original.c
+++ p1original.c
@@ -1,2 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+  printf("Enter coordinates of point1\n");
+  scanf("%f%f", x1, y1);
+  printf("Enter coordinates of point2\n");
+  scanf("%f%f", x2, y2);
+}
 
-test
+void find_distance(float x1, float y1, float x2, float y2, float *d)
+{
+  *d = sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+
+void output(float x1, float y1, float x2, float y2, float d)
+{
+  printf("The distance between %f,%f and %f,%f is %f\n",x1,y1,x2,y2,d);
+}
+
+int main()
+{
+  float a1, a2, b1, b2, res;
+  input(&a1,&b1,&a2,&b2);
+  find_distance(a1,b1,a2,b2,&res);
+  output(a1,b1,a2,b2,res);
+  return 0;
+}
