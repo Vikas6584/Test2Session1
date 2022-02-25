6db03e174fd706831e87e714b1f362d259d22f6a
Vikas6584 <69501909+Vikas6584@users.noreply.github.com>
2022-02-25 16:35:43 +0000

    fixed :))

---
+++ p6original.c
@@ -0,0 +1,25 @@
+
+int string_length(char *s)
+{
+  int i;
+  for(i=0;s[i]!='\0';i++);
+  return i;
+}
+
+int string_ncmp(char *s1,char *s2)
+{
+  for(int i=0; i<n && s1[i]==s2[i] && s1[i] != '\0';i++);
+  return s1[i] - s2[i];
+}
+
+int string_index(char *s, char *subs)
+{
+  int l1=string_lenght(s);
+  int l2=string_lenght(subs);
+  if l1 < l2
+      return -1;
+  for(int i=0;i<l1-l2;i++)
+    if (string_nscmp(s,subs))
+      return i;
+  return -1;
+}
